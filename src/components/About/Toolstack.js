de0ad82fe15b6a281e071ec0b9e9c979d266b0fa
PC <rishabhsharawat150@gmail.com>
2023-04-30 01:02:39 +0530

    first commit

---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiLinux,
+  SiVisualstudiocode,
+  SiPostman,
+  SiHeroku,
+  SiPycharm,
+  SiJupyter,
+  SiMysql,
+} from "react-icons/si";
+import {AiFillCodeSandboxCircle} from "react-icons/ai";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiLinux />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiJupyter />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPycharm />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiHeroku />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMysql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <AiFillCodeSandboxCircle/>
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
