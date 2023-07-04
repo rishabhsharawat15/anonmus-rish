b3a138b79cde69de90fc4b80a048e4540e77fda9
PC <rishabhsharawat150@gmail.com>
2023-07-04 21:54:17 +0530

    new changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
+import keeper from "../../Assets/Projects/keeper.jpeg";
+import motiondetector from "../../Assets/Projects/motiondetector.jpeg"
 import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
+import mytodo from "../../Assets/Projects/mytodo.jpeg";
 import bitsOfCode from "../../Assets/Projects/blog.png";
 
 function Projects() {
@@ -25,10 +25,10 @@ function Projects() {
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              title="Chatbot (currently working)"
+              description="This Python-based chatbot is a versatile and interactive conversational AI system designed to facilitate seamless communication with users. Built using natural language processing techniques, the chatbot leverages an intent file to adapt its responses to different user inputs. This intent file acts as a repository, containing predefined user intents and corresponding responses, enabling the chatbot to accurately comprehend user queries and provide contextually relevant answers"
               ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              //deomolink
             />
           </Col>
 
@@ -36,7 +36,7 @@ function Projects() {
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="Bits-0f-C0de"
+              title="DUTY MUNSHI"
               description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
               ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
               demoLink="https://blogs.soumya-jit.tech/"
@@ -47,7 +47,7 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
+              title="IN NEED"
               description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
               ghLink="https://github.com/soumyajit4419/Editor.io"
               demoLink="https://editor.soumya-jit.tech/"              
@@ -56,37 +56,35 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={motiondetector}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Motion detection"
+              description="Introduction Motion detection using Python is easy because of the multiple open-source libraries provided by the Python programming language which detect even slight motion in the frame and highlight it with green rectangle frame and the time the object entered and the time, it exited the frame is recorded and using bokeh library of python and python compatibility with other languages make it possible for to plot with html. For example, it can be used for invigilation in online exams or for security purposes in stores, banks, etc"
+              ghLink="https://github.com/rishabhsharawat15/objectdetection"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={keeper}
+              isBlog={false}
+              title="KEEPER APP"
+              description="Keeper is an innovative notepad-like application designed to help users organize and manage their notes efficiently. It provides a simple and intuitive interface for creating, editing, and organizing text-based notes, making it easier to keep track of important information"
+              ghLink="https://github.com/rishabhsharawat15/KEEPER-APP"
+              demoLink="https://fni4t6.csb.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={mytodo}
+              isBlog={false}
+              title="TO DO APP"
+              description="To-do app is used to enhance productivity and efficiency by providing a centralized platform for capturing, organizing, and completing tasks.made todo using python/rect/ejs"
+              ghLink="https://github.com/rishabhsharawat15/My-todo"
+              demoLink =" https://rishabhsharawat15-my-todo-web-stzzj2.streamlit.app/"
+             
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col>
         </Row>
       </Container>
     </Container>
